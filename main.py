f849c681abb25c890fde1105389c95159ed18b7a
swild78 <swild78@wgu.edu>
2026-07-15 12:24:38 -0400

    Step 4.1: Initial Training

--- main.py
+++ main.py
@@ -79,9 +79,16 @@ def go(config: DictConfig):
             pass
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
+            _ = mlflow.run(
+                os.path.join(config["main"]["components_repository"], "train_val_test_split"),
+                "main",
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": str(config["modeling"]["test_size"]),
+                    "random_seed": str(config["modeling"]["random_seed"]),
+                    "stratify_by": config["modeling"]["stratify_by"]
+                },
+            )
             pass
 
         if "train_random_forest" in active_steps:
